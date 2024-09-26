5651988ccdd272fbda3c239fc5ec4bedffc51e52
Emiliano Sanchez <emiliano.sanchez3310@gmail.com>
2024-09-26 15:55:01 -0700

    first commit

---
+++ coffee.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup
+from plyer import notification
+import json
+import csv
+import pickle
+import pandas as pd
+import time
+temp_data = []
+
+def clean(ds):
+    ds.dropna(inplace=True)
+    ds.drop_duplicates(inplace=True)
+    return ds
+
+
+def extract_text(x):
+    ed = requests.get(x)
+    ed_soup = BeautifulSoup(ed.content, 'html.parser')
+    extracted_text = []
+    for line in ed_soup.find_all('p'):
+        text = line.text.strip()
+        if text:
+            extracted_text.append(text)
+
+    return extracted_text
+
+
+def extract_links(y):
+    ed = requests.get(y)
+    ed_soup = BeautifulSoup(ed.content, 'html.parser')
+    extracted_links = []
+    for link in ed_soup.find_all('a'):
+        href = link.get('href')
+        if href:
+            extracted_links.append(href)
+
+    return extracted_links
+
+def extract_all(z):
+    output = requests.get(z)
+    ed_soup = BeautifulSoup(output.content, 'html.parser')
+    extracted_links = []
+    for link in ed_soup.find_all('a'):
+        href = link.get('href')
+        if href:
+            extracted_links.append(href)
+            temp_data.append(extracted_links)
+    
+    ed = requests.get(z)
+    ed_soup = BeautifulSoup(ed.content, 'html.parser')
+    extracted_text = []
+    for line in ed_soup.find_all('p'):
+        text = line.text.strip()
+        if text:
+            extracted_text.append(text)
+            temp_data.append(extracted_text)
+
+    return temp_data
+
+    
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='coffee_scrape',
+    version='0.1.0',
+    description='A simple Python library designed to simplify web scraping',
+    author='Emiliano Sanchez',
+    author_email='emiliano.sanchez3310@gmail.com',
+    packages=['coffee_scrape'],
+)
